a773a888327cc8e7fca2fa892db3be478d40a0b2
nohheeyeon <heeyeon1213@gmail.com>
2025-07-20 22:52:54 +0900

    chore: 프로젝트 생성 및 초기 환경설정

---
+++ src/main/java/com/myjourneylog/MyJourneyLogApplication.java
@@ -0,0 +1,13 @@
+package com.myjourneylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJourneyLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJourneyLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myjourneylog/MyJourneyLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjourneylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJourneyLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
