654396ecdfd9ef6a52a67b4e37e72351ef2dede8
AMinSC <cdtjdcjf@gmail.com>
2025-07-23 00:14:41 +0900

    Add gemini api config and web securuty config

--- src/main/java/com/myjourneylog/config/GeminiConfig.java
+++ src/main/java/com/myjourneylog/config/GeminiConfig.java
@@ -1,4 +1,34 @@
 package com.myjourneylog.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GeminiConfig {
+
+    @Value("${gemini.api-key}")
+    private String apiKey;
+
+    @Value("${gemini.api-url}")
+    private String apiUrl;
+
+    @Bean
+    public UriComponents fullApiUrl() {
+        // API URL에 API 키를 쿼리 파라미터로 추가
+        return UriComponentsBuilder.fromUriString(apiUrl)
+                .queryParam("key", apiKey)
+                .build();
+    }
 }
