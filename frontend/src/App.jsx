834f7ddad0196e96a47b3dad9017d5743b260bc9
nohheeyeon <heeyeon1213@gmail.com>
2025-07-29 23:31:24 +0900

    feat : 프론트-백엔드 로그인 연동 및 프록시 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import KakaoMap from "./components/KakaoMap";
+import { useState } from "react";
+import Login from "./components/Login";
 
 function App() {
+    const [user, setUser] = useState(null);
+
+    if (!user) {
+        return <Login onLogin={setUser} />;
+    }
+
     return (
         <div>
-            <h1 style={{ textAlign: "center", marginTop: 24 }}>Kakao Map</h1>
-            <KakaoMap />
+            <h1>안녕하세요, {user.nickname || user.email}!</h1>
+            {/* 로그인 후 메인화면 */}
         </div>
     );
 }
